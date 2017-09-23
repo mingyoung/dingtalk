d54431ab01c52107f0f18e47d45e2f102d6bb25b
MINGYOUNG <mingyoungcheung@gmail.com>
2017-09-23 13:06:03 +0800

    Add tests.

---
+++ tests/TestCase.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the mingyoung/dingtalk.
+ *
+ * (c) mingyoung <mingyoungcheung@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EasyDingTalk\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+}

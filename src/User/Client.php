fc290827bea42d132140624aeae510a7f3d08e00
nadirvishun <nadirvishun@gmail.com>
2018-05-11 15:29:25 +0800

    通过CODE换取用户身份的接口
    在免登授权时会返回code，然后通过code用此接口获取userid，再进行下一步的操作

--- src/User/Client.php
+++ src/User/Client.php
@@ -129,4 +129,14 @@ class Client extends BaseClient
     {
         return $this->httpGet('user/get_org_user_count', $params);
     }
+    
+    /**
+     * @param string $code
+     *
+     * @return array
+     */
+    public function count(string $code)
+    {
+        return $this->httpGet('user/getuserinfo', ['code' => $code]);
+    }
 }

92c2a2458207731c946c5b5ea1396146760ed5b1
baijunyao <baijunyao@baijunyao.com>
2018-05-23 14:31:44 +0800

    增加微应用管理功能

---
+++ src/Microapp/ServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the mingyoung/dingtalk.
+ *
+ * (c) baijunyao <baijunyao@baijunyao.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EasyDingTalk\Microapp;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+/**
+ * Class ServiceProvider.
+ *
+ * @author baijunyao <baijunyao@baijunyao.com>
+ */
+class ServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $app)
+    {
+        $app['microapp'] = function ($app) {
+            return new Client($app);
+        };
+    }
+}
